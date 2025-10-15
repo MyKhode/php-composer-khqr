8fb9f7d49f7640f1432eb1faa06c62b975a29a1f
Ikhode Studio <132768132+MyKhode@users.noreply.github.com>
2025-10-15 13:20:38 +0700

    Update phinx.php

--- phinx.php
+++ phinx.php
@@ -1,20 +1,24 @@
 <?php
+
 return [
-'paths' => [
-'migrations' => 'migrations'
-],
-'environments' => [
-'default_migration_table' => 'phinxlog',
-'default_environment' => 'development',
-'development' => [
-'adapter' => 'mysql',
-'host' => getenv('DB_HOST') ?: '127.0.0.1',
-'name' => getenv('DB_NAME') ?: 'khqr_store',
-'user' => getenv('DB_USER') ?: 'root',
-'pass' => getenv('DB_PASS') ?: 'ikhode',
-'port' => getenv('DB_PORT') ?: 4987,
-'charset' => 'utf8mb4',
-],
-],
-'version_order' => 'execution'
+    'paths' => [
+        'migrations' => 'migrations',
+        'seeds' => 'seeds',
+    ],
+
+    'environments' => [
+        'default_migration_table' => 'phinxlog',
+        'default_environment' => 'development',
+        'development' => [
+            'adapter' => 'mysql',
+            'host' => getenv('DB_HOST') ?: 'mysql',
+            'name' => getenv('DB_NAME') ?: 'khqr_store',
+            'user' => getenv('DB_USER') ?: 'root',
+            'pass' => getenv('DB_PASS') ?: 'ikhode',
+            'port' => getenv('DB_PORT') ?: 3306,
+            'charset' => 'utf8mb4',
+        ],
+    ],
+
+    'version_order' => 'execution',
 ];
